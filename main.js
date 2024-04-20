49ccdee1351e6952866ed0485b07bbf77c646aa5
GraySS <klinkemalliesasha@gmail.com>
2024-04-20 18:25:07 +0200

    truc

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.set("views", path.join(__dirname, 'static', 'views'))
+app.use(express.static('static'));
+app.set("views", path.join(__dirname, 'static', 'views'));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
