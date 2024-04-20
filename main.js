c9f00b77d9208d8e4603bf50c8257cd1fcfe7b7d
maverhasselt <martin.verhasselt@student.uclouvain.be>
2024-04-20 18:17:59 +0200

    ADD

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var login = require("./USERJS/login")
+app.use("/login", login)
+
 app.get('/', (req, res) => {
     res.send("Base")
 })
