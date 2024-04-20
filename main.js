dad50822d0ea8e080f56f3903f00087343e59b1b
maverhasselt <martin.verhasselt@student.uclouvain.be>
2024-04-20 18:23:21 +0200

    ADD

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 app.set("views", path.join(__dirname, 'static', 'views'))
 app.set("view engine", "ejs");
-app.use("")
 var login = require("./USERJS/login")
 app.use("/login", login)
 
