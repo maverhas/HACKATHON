eeda366df945a4b14eb991b779faff29f51fe92b
maverhasselt <martin.verhasselt@student.uclouvain.be>
2024-04-21 03:01:08 +0200

    Ajout

--- routers/level2.js
+++ routers/level2.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+router.get('/course', (req, res) => {
+    res.render('levels/2/course.ejs')
+})
 router.get('/', (req, res) => {
 
     console.log(req.session.level);
@@ -11,5 +13,6 @@ router.get('/', (req, res) => {
 
     res.render('levels/2/index.ejs');
 });
+
 
 module.exports = router;
